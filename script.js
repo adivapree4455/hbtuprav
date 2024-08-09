8224566954ec57e8878f343a95bc7633720bf508
preetiadi <adivapreeti@gmail.com>
2024-08-09 19:26:59 +0530

    completed

---
+++ script.js
@@ -0,0 +1,19 @@
+anime.timeline({
+    loop: true
+})
+.add ({
+    targets: '.message .cube',
+    scale: [2, 1],
+    opacity: [0, 1],
+    easing: 'easeOutExpo',
+    duration: 950,
+    delay: (el, i) => 70*i
+})
+.add ({
+    targets: '.message .cube',
+    scale: [1, 0],
+    opacity: 0,
+    duration: 950,
+    easing: 'easeInQuad',
+    delay: (el, i) => 70*i
+})
